3d5d9ded8ecbbd7b40fd65248fb52cb9a38eeb28
thinkAmi <dev.thinkami+git@gmail.com>
2022-12-11 23:01:09 +0900

    add OpenAPI schema for @action

--- api/views.py
+++ api/views.py
@@ -1,9 +1,10 @@
-from rest_framework import viewsets
+from drf_spectacular.utils import extend_schema, extend_schema_view, inline_serializer, OpenApiExample, OpenApiResponse
+from rest_framework import viewsets, serializers
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from api.serializers import ShopSerializer
 from shop.models import Shop
-
-from drf_spectacular.utils import extend_schema, extend_schema_view
 
 
 @extend_schema_view(
@@ -17,8 +18,34 @@ from drf_spectacular.utils import extend_schema, extend_schema_view
                 'payloadFormatVersion': '1.0'
             }
         }
+    ),
+    message=extend_schema(
+        operation_id='shop_status',
+        responses={
+            200: OpenApiResponse(
+                response=inline_serializer(
+                    name='MessageResponse',
+                    fields={
+                        'message': serializers.CharField(),
+                    },
+                ),
+                description='レスポンス型'
+            )
+        },
+        examples=[OpenApiExample(
+            name='例',
+            value=[
+                {'message': 'open'},
+                {'message': 'close'}
+            ]
+        )],
+        description='actionデコレータ分'
     )
 )
 class ShopViewSet(viewsets.ModelViewSet):
     queryset = Shop.objects.all()
     serializer_class = ShopSerializer
+
+    @action(detail=False, methods=['get'])
+    def message(self, request, pk=None):
+        return Response({'message': 'open'})
