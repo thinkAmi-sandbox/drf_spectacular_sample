dfb23e1e0cb4c4eaefeebfc2d479707e2f6c6910
thinkAmi <dev.thinkami+git@gmail.com>
2022-12-11 12:03:22 +0900

    camelize operationId

--- config/settings.py
+++ config/settings.py
@@ -132,3 +132,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
+
+SPECTACULAR_SETTINGS = {
+    'CAMELIZE_NAMES': True,
+}
