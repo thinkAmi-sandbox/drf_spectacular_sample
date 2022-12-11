ab811b06facfe0610bf32e8ef0b67305ab5a1da7
thinkAmi <dev.thinkami+git@gmail.com>
2022-12-11 17:14:42 +0900

    add extensions

--- api/views.py
+++ api/views.py
@@ -8,7 +8,15 @@ from drf_spectacular.utils import extend_schema, extend_schema_view
 
 @extend_schema_view(
     list=extend_schema(
-        operation_id='shops'
+        operation_id='shops',
+        extensions={
+            'x-amazon-apigateway-integration': {
+                'type': 'AWS_PROXY',
+                'httpMethod': 'POST',
+                'uri': 'arn:aws:lambda:***:***:function:HelloWorld',
+                'payloadFormatVersion': '1.0'
+            }
+        }
     )
 )
 class ShopViewSet(viewsets.ModelViewSet):
