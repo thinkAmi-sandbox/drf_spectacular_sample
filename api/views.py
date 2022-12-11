26d61cf4216155eaa30a0e9c977cb181bec9107d
thinkAmi <dev.thinkami+git@gmail.com>
2022-12-11 16:59:23 +0900

    replace operationId

--- api/views.py
+++ api/views.py
@@ -3,7 +3,14 @@ from rest_framework import viewsets
 from api.serializers import ShopSerializer
 from shop.models import Shop
 
+from drf_spectacular.utils import extend_schema, extend_schema_view
 
+
+@extend_schema_view(
+    list=extend_schema(
+        operation_id='shops'
+    )
+)
 class ShopViewSet(viewsets.ModelViewSet):
     queryset = Shop.objects.all()
     serializer_class = ShopSerializer
